ea53c8bc5c96f96d8f411648eef7d74fb7a84657
ihor <i0962112822@gmail.com>
2024-05-18 00:01:26 +0300

    add

---
+++ js/data.js
@@ -0,0 +1,110 @@
+const data = [
+  {
+    img: "./img/symbol-defs.svg#icon-tap",
+    title: "Plumbing",
+    services: [
+      "Install / Replace a Toilet",
+      "Install / Replace a Faucet",
+      "Fix a Leaking",
+      "Install / Replace a Sink",
+      "Repair Under Sink Plumbing",
+    ],
+    color: "green",
+    link: "View more",
+  },
+  {
+    img: "./img/symbol-defs.svg#icon-paint",
+    title: "Drywall & painting",
+    services: [
+      "Install Drywall",
+      "Patch and Paint Drywall",
+      "Paint a Wall / Trim",
+      "Paint or Stain A Fence",
+      "Paint a Cabinet Door",
+    ],
+    color: "blue",
+    link: "View more",
+  },
+  {
+    img: "./img/symbol-defs.svg#icon-sofa",
+    title: "Furniture assembly",
+    services: [
+      "Assemble Fitness Equipment",
+      "Assemble - Custom Request",
+      "Assemble a TV Stand",
+      "Assemble Pet Furniture",
+      "Assemble a Bar Cabinet",
+    ],
+    color: "yellow",
+    link: "View more",
+  },
+  {
+    img: "./img/symbol-defs.svg#icon-Electrical",
+    title: "Electrical",
+    services: [
+      "Install / Replace a Ceiling Fan",
+      "Replace Switch & Outlet Faceplate",
+      "Upgrade to a Video Doorbell",
+      "Install / Replace an Bathroom Exhaust Fan",
+    ],
+    color: "red",
+    link: "View more",
+  },
+  {
+    img: "./img/symbol-defs.svg#icon-door",
+    title: "Doors & windows",
+    services: [
+      "Install / Replace New Door",
+      "Install / Replace a Screen Door",
+      "Install / Replace a Window Screen",
+      "Repair a Door Frame",
+    ],
+    color: "green",
+    link: "View more",
+  },
+  {
+    img: "./img/symbol-defs.svg#icon-picture",
+    title: "Hanging & mounting",
+    services: [
+      "Mount a TV",
+      "Remove a TV Mount from Wall",
+      "Mount and Hang a Projector Screen",
+      "Hang a Bathroom Fixture",
+    ],
+    color: "blue",
+    link: "View more",
+  },
+  {
+    img: "./img/symbol-defs.svg#icon-battery",
+    title: "Appliance installation",
+    services: ["Replace an Existing Dishwasher", "Replace an Existing Microwave", "Replace an Existing Oven "],
+    color: "yellow",
+    link: "View more",
+  },
+  {
+    img: "./img/symbol-defs.svg#icon-house",
+    title: "Smart home",
+    services: [
+      "Install a Smart Lock",
+      "Install a Smart Home Security Camera",
+      "Mount a Baby Monitor Camera on the Wall",
+      "Install an Under Sink Water Filtration System",
+    ],
+    color: "red",
+    link: "View more",
+  },
+  {
+    img: "../img/symbol-defs.svg#icon-windows",
+    title: "Outdoor",
+    services: [
+      "Assemble an Outdoor Trampoline",
+      "Hang a Garage Shelving Unit",
+      "Install Outdoor Decorative Exterior Window Shutters",
+      "Install a Storm Door",
+    ],
+    color: "green",
+    link: "View more",
+  },
+];
+
+export default data;
---
+++ js/index.js
@@ -0,0 +1,25 @@
+import data from "./data.js";
+
+const wrapper = document.querySelector(".section");
+
+const markup = `
+  ${data
+    .map(
+      ({ img, title, services, color }) => `
+    <div class="card ${color}">
+      <svg class="svg" width="110px" height="100px">
+        <use href="${img}"></use>
+      </svg>
+      <div> <h2 class="title">${title}</h2>
+      <ul class="list>
+        ${services.map((el) => `<li class="item"><p class="text">${el}</p></li>`).join("")}
+      </ul></div>
+     
+      <a href="#" class="link">View more</a>
+    </div>
+  `
+    )
+    .join("")}
+`;
+
+wrapper.innerHTML = markup;
